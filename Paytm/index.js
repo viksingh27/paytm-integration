6fd132289058923da7b9f6cf8935947aba8736f8
Vikrant Singh <60031777+viksingh27@users.noreply.github.com>
2022-01-11 02:36:14 +0530

    Update index.js

--- Paytm/index.js
+++ Paytm/index.js
@@ -19,12 +19,12 @@ app.post("/paynow", [parseUrl, parseJson], (req, res) => {
   var paymentDetails = {
     orderID: req.body.id,
     amount: req.body.amount,
-    customerId: req.body.name,
+    customer_Id: req.body.name,
     customerEmail: req.body.email,
     customerPhone: req.body.phone,
    
 }
-if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.customerEmail || !paymentDetails.customerPhone) {
+if(!paymentDetails.amount || !paymentDetails.customer_Id || !paymentDetails.customerEmail || !paymentDetails.customerPhone) {
     res.status(400).send('Payment failed')
 } else {
     var params = {};
@@ -33,7 +33,7 @@ if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.custo
     params['CHANNEL_ID'] = 'WEB';
     params['INDUSTRY_TYPE_ID'] = 'Retail';
     params['ORDER_ID'] = 'TEST_'  + paymentDetails.orderID;
-    params['CUST_ID'] = paymentDetails.customerId;
+    params['CUST_ID'] = paymentDetails.customer_Id;
     params['TXN_AMOUNT'] = paymentDetails.amount;
     params['CALLBACK_URL'] = 'https://developerpayment.herokuapp.com/callback';
     params['EMAIL'] = paymentDetails.customerEmail;
