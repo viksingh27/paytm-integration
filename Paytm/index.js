a65553decb9905d67222eccc7815b59995cba17e
Vikrant Singh <60031777+viksingh27@users.noreply.github.com>
2022-01-11 04:47:41 +0530

    Update index.js

--- Paytm/index.js
+++ Paytm/index.js
@@ -35,7 +35,7 @@ if(!paymentDetails.amount || !paymentDetails.customerEmail || !paymentDetails.cu
     params['ORDER_ID'] = 'TEST_'  + paymentDetails.orderID;
 //     params['CUST_ID'] = 'CUST_' + paymentDetails.customer_Id;
     params['TXN_AMOUNT'] = paymentDetails.amount;
-    params['CALLBACK_URL'] = 'localhost:4100/callback';
+    params['CALLBACK_URL'] = 'http://localhost:4100/callback';
     params['EMAIL'] = paymentDetails.customerEmail;
     params['MOBILE_NO'] = paymentDetails.customerPhone;
   
