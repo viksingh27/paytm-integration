8838d814e170fb1de2ca023ce6b154553eaea247
Vikrant Singh <60031777+viksingh27@users.noreply.github.com>
2022-01-12 01:22:59 +0530

    Update index.js

--- Paytm/index.js
+++ Paytm/index.js
@@ -33,7 +33,7 @@ app.post('/paynow', [parseUrl, parseJson], (req, res) => {
     params['ORDER_ID'] = 'TEST_'  + paymentDetails.orderID;
 //     params['CUST_ID'] = 'customer_001';
     params['TXN_AMOUNT'] = paymentDetails.amount;
-    params['CALLBACK_URL'] = 'http://localhost:4100/callback';
+    params['CALLBACK_URL'] = 'https://developerpayment.herokuapp.com/callback';
     params['EMAIL'] = paymentDetails.customerEmail;
     params['MOBILE_NO'] = req.body.phone.toString();
 
