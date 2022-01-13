3e4eff5cdeac5554c4799cb90dea1ac4d61e2b17
Vikrant Singh <60031777+viksingh27@users.noreply.github.com>
2022-01-13 22:21:15 +0530

    Update index.js

--- Paytm/index.js
+++ Paytm/index.js
@@ -126,7 +126,7 @@ app.post("/callback", (req, res) => {
            var _result = JSON.parse(response);
              if(_result.STATUS == 'TXN_SUCCESS') {
                  console.log("^^^^^^^",_result)
-                res.redirect(`https://competent-easley-4d184f.netlify.app/viewOrder?status=${_result.STATUS}&ORDERID=${_result.ORDERID}&date=${_result.TXNDATE}&bank=${_result.BANKNAME}`)
+                res.redirect(`https://xenodochial-blackwell-783256.netlify.app/viewOrder?status=${_result.STATUS}&ORDERID=${_result.ORDERID}&date=${_result.TXNDATE}&bank=${_result.BANKNAME}`)
              }else {
                  res.send('payment failed')
              }
